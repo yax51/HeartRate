2373355ac4fd31bbefe8240b8b8aebf384583e04
yax51 <40424386+yax51@users.noreply.github.com>
2018-08-13 01:23:33 -0700

    Add files via upload

---
+++ Rate.cs
@@ -0,0 +1,64 @@
+﻿//Heart rate class
+
+class HeartRate
+{
+    private int currentYear;
+
+    public HeartRate() : this("Patrick ", "Davis", 1982, 2018) { }
+
+    public HeartRate(string firstName, string lastName, int birthYear, int currentYear)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.BirthYear = birthYear;
+        this.currentYear = currentYear;
+        
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int BirthYear { get; set; }
+    public int CurrentYear { get; set; }
+    public int Old { get; set; }
+
+    
+    public int GetAge()
+    {
+        return (currentYear - BirthYear);
+    }
+
+    private int calcRate()
+    {
+        return (220 - GetAge());
+                
+    }
+    public int MinRate()
+    {
+        return (calcRate() / 2);
+    }
+
+    public double MaxRate()
+    {
+        return (calcRate() * 0.85);
+    }
+
+    public string FName()
+    {
+        return FirstName;
+
+    }
+    
+    public string LName()
+    {
+        return LastName;
+    }
+
+    public int old()
+    {
+        return (currentYear - BirthYear);
+    }
+    
+}
+
+
+  
---
+++ RateTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ConsoleApp5
+
+{
+
+    class RateTest
+    {
+        static void Main(string[] args)
+        {
+            HeartRate me = new HeartRate();
+            System.Windows.Forms.MessageBox.Show("Maximum Heart Rate is {0}. ", me.MaxRate());
+            Console.WriteLine("Minimum Heart Rate is {0}. ", me.MinRate());
+            Console.WriteLine("Your Name is: {0} ", me.FirstName + me.LastName);
+            Console.WriteLine("You are {0} years old", me.GetAge());
+                       
+
+        }
+    }
+
+}
